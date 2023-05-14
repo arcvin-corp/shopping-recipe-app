f740a62696e2b1666e1f1b105a447dd598f53303
arcvin <arcvin.corp@gmail.com>
2023-05-14 20:16:51 +0530

    Completed error handling for shopping edit component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Alert, AlertLevels } from '../shared/alerts.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,9 +12,26 @@ export class ShoppingListComponent {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 3),
   ];
+  alert: Alert;
+  isVisible = false;
 
   addIngredient(ingredient: Ingredient): void {
-    this.ingredients.push(ingredient);
-    console.log(this.ingredients);
+    let ingredientName = ingredient.name.toLocaleLowerCase().trim();
+    if (
+      this.ingredients.some((ing) => ing.name.toLowerCase() === ingredientName)
+    ) {
+      this.isVisible = true;
+      this.alert = new Alert(
+        `The ingredient '${ingredientName}' is already added.`,
+        AlertLevels.ERROR
+      );
+    } else {
+      this.isVisible = false;
+      this.ingredients.push(ingredient);
+    }
+  }
+
+  closeAlert() {
+    this.isVisible = false;
   }
 }
