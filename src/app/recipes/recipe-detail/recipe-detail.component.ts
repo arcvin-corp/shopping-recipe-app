873e35e5d466e084de13c4a1b1908fddac95bba4
arcvin <arcvin.crop@gmail.com>
2023-05-21 09:46:59 +0530

    WIP

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
@@ -8,12 +17,18 @@ import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css'],
 })
-export class RecipeDetailComponent implements OnChanges {
+export class RecipeDetailComponent implements OnChanges, AfterViewInit {
   @Input() recipeDetails: Recipe;
+  @ViewChild('selectAllCheckBox') selectAllCheckBoxElRef: ElementRef;
+  selectAllCheckBoxEl: HTMLInputElement;
   selectedIngredients: Ingredient[] = [];
   isAllSelected: boolean = false;
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  ngAfterViewInit(): void {
+    this.selectAllCheckBoxEl = this.selectAllCheckBoxElRef.nativeElement;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.selectedIngredients = [];
@@ -30,7 +45,9 @@ export class RecipeDetailComponent implements OnChanges {
         : this.selectedIngredients.splice(indexValue, 1);
     }
 
-    console.log(this.selectedIngredients);
+    console.log(this.isAllSelected);
+    this.selectedIngredients.length === 0 &&
+      (this.selectAllCheckBoxEl.checked = !this.selectAllCheckBoxEl.checked);
   }
 
   addIngToShoppingList() {
@@ -39,8 +56,11 @@ export class RecipeDetailComponent implements OnChanges {
     );
   }
 
-  onSelectAll() {
-    this.isAllSelected = !this.isAllSelected;
-    this.selectedIngredients = [...this.recipeDetails.ingredients];
+  onSelectAll(e: Event) {
+    (<HTMLInputElement>e.target).checked
+      ? ((this.isAllSelected = true),
+        (this.selectedIngredients = [...this.recipeDetails.ingredients]))
+      : ((this.isAllSelected = false), (this.selectedIngredients = []));
+    console.log(this.selectedIngredients);
   }
 }
